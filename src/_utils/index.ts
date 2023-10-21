6b01ee8de4d4b525b57152807e7f2aeb25a8a3fa
Agus Garcia <agusss.garcia19@gmail.com>
2023-10-21 19:50:32 -0300

    feat(execute): add utils

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -1,3 +1,4 @@
 export { default as createFolder } from "./createFolder";
+export { default as execute } from "./execute";
 export { default as exists } from "./exists";
 export { default as IgnorableError } from "./IgnorableError";
