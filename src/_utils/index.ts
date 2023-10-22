730c4ba24ce9ab47a691d5f2e5b55c3086676d68
Agus Garcia <agusss.garcia19@gmail.com>
2023-10-22 17:12:19 -0300

    feat(remove): add utils

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -4,3 +4,4 @@ export { default as exists } from "./exists";
 export { default as IgnorableError } from "./IgnorableError";
 export { default as isLibrary } from "./isLibrary";
 export { default as readFile } from "./readFile";
+export { default as remove } from "./remove";
