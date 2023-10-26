4b5e1eaca497ff7a148a1658ea0cb2199a92be19
Agus Garcia <agusss.garcia19@gmail.com>
2023-10-26 16:30:04 -0300

    feat(IgnorableError): add class

---
+++ src/_utils/IgnorableError.ts
@@ -0,0 +1,3 @@
+export default class IgnorableError {
+  readonly ignorable: true = true;
+}
--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -1,2 +1,3 @@
 export { default as createFolder } from "./createFolder";
 export { default as exists } from "./exists";
+export { default as IgnorableError } from "./IgnorableError";
