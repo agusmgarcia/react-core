8232b5908718d7f63d5a19465e6fc53adffa9393
Agus Garcia <agusss.garcia19@gmail.com>
2023-10-27 13:14:29 -0300

    fix(build.core): split commands

--- src/core/build.core.ts
+++ src/core/build.core.ts
@@ -1,8 +1,11 @@
 import { execute } from "../_utils";
 import withContext from "./withContext.core";
 
-const COMMAND = "del .next out && next build --no-lint";
+const COMMAND_1 = "del .next out";
+const COMMAND_2 = "next build --no-lint";
 
 export default function build(): Promise<void> {
-  return withContext(() => execute(COMMAND, true));
+  return withContext(() =>
+    execute(COMMAND_1, true).then(() => execute(COMMAND_2, true)),
+  );
 }
