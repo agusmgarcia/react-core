35fb8cc52b70089f9f54d7d8c0dde2f6441f6091
Agus Garcia <agusss.garcia19@gmail.com>
2024-05-23 10:18:53 -0300

    chore: bump peer dependencies

--- src/eslint/format.eslint.ts
+++ src/eslint/format.eslint.ts
@@ -2,7 +2,7 @@ import { execute } from "../_utils";
 import withContext from "./withContext.eslint";
 
 const COMMAND =
-  "next lint --cache --cache-location ./node_modules/.eslintcache --cache-strategy content --dir . --fix";
+  "next lint --cache-location ./node_modules/.eslintcache --cache-strategy content --dir . --fix";
 
 export default function format(options: { skip: string[] }): Promise<void> {
   return withContext(() => execute(COMMAND, true), options);
