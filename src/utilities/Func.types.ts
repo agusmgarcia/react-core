92ecf896ec7739cb2f63aa13c729ad213ab74066
Agus Garcia <agusss.garcia19@gmail.com>
2024-07-15 16:00:02 -0300

    chore(Func.types): make typescript compatible

--- src/utilities/Func.types.ts
+++ src/utilities/Func.types.ts
@@ -1,9 +1,17 @@
-type Func<TResult = void, TArgs extends any[] = []> = TResult extends any[]
-  ? TArgs extends []
-    ? TResult extends [...infer TArgs, infer TResult]
-      ? (...args: TArgs) => TResult
-      : never
-    : (...args: TArgs) => TResult
-  : (...args: TArgs) => TResult;
+type Func<
+  TResult = void,
+  TArgs extends any[] | undefined = undefined,
+> = TArgs extends undefined
+  ? TResult extends [...infer TArgs, infer TResult]
+    ? (...args: TArgs) => TResult
+    : () => TResult
+  : TArgs extends any[]
+    ? (...args: TArgs) => TResult
+    : never;
+
+// TODO: use this definition on the next major release.
+// type Func<TResult = void, TArgs extends any[] = []> = (
+//   ...args: TArgs
+// ) => TResult;
 
 export default Func;
