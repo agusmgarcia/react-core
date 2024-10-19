0c58556904321a13c7adef5c9cc038914692d319
Agus Garcia <agusss.garcia19@gmail.com>
2024-10-19 15:55:44 -0300

    fix(nextjs.middleware): adjust nextjs config file

--- src/_bin/middlewares/nextjs.middleware.ts
+++ src/_bin/middlewares/nextjs.middleware.ts
@@ -41,7 +41,7 @@ export default async function nextJSMiddleware(
 
 const nextConfig = `const { PHASE_PRODUCTION_BUILD } = require("next/constants");
 
-/** @type {import('next').NextConfig} */
+/** @type import('next').NextConfig */
 module.exports = (phase) => ({
   basePath: process.env.BASE_PATH,
   output: phase === PHASE_PRODUCTION_BUILD ? "export" : undefined,
