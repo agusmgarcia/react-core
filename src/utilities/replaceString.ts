1fa210dc89c04de3cda0ea311fe5271722809e18
Agus Garcia <agusss.garcia19@gmail.com>
2024-10-31 12:17:40 -0300

    fix(replaceString): handle scenarios for undefined message

--- src/utilities/replaceString.ts
+++ src/utilities/replaceString.ts
@@ -1,25 +1,41 @@
 const searchValue = /\$\{(.+?)\}/g;
 const multipleValues = /^(.+?)\:(.+?)\/(.+?)$/;
 
+type Replacements = Record<string, string | number | boolean | undefined>;
+
 export default function replaceString(
   message: string,
-  replacements: Record<string, string | number | boolean | undefined>,
-): string {
+  replacements?: Replacements,
+): string;
+
+export default function replaceString(
+  message: string | undefined,
+  replacements?: Replacements,
+): string | undefined;
+
+export default function replaceString(
+  message: string | undefined,
+  replacements?: Replacements,
+): string | undefined {
+  if (message === undefined) return undefined;
+
   return message.replace(searchValue, (original, key) => {
     if (multipleValues.test(key)) {
       const matches = multipleValues.exec(key);
       if (matches === null) return original;
 
-      const replacer = replacements[matches[1]];
+      const replacer = replacements?.[matches[1]];
       if (typeof replacer === "number")
         return replacer === 1 ? matches[2] : matches[3];
+
       return original;
     }
 
-    const replacer = replacements[key];
+    const replacer = replacements?.[key];
     if (typeof replacer === "string") return replacer;
     if (typeof replacer === "number") return replacer.toString();
     if (typeof replacer === "boolean") return replacer.toString();
+
     return original;
   });
 }
