e9a5b01d44a7fa9e1e029ef6d5391677dbf5bb53
Agus Garcia <agusss.garcia19@gmail.com>
2024-11-02 10:41:32 -0300

    feat(replaceString)!: remove the old-way

--- src/utilities/replaceString.ts
+++ src/utilities/replaceString.ts
@@ -1,6 +1,5 @@
 const searchValue = /\$\{(.+?)\}/g;
-const multipleValues = /^(.+?)\:(.+?)\/(.+?)$/; // TODO: delete this regexp in the next major version.
-const multipleValuesV3 = /^(.+?)\?(.+?)\:(.+?)$/;
+const multipleValues = /^(.+?)\?(.+?)\:(.+?)$/;
 
 type Replacements = Record<string, string | number | boolean | undefined>;
 
@@ -21,28 +20,11 @@ export default function replaceString(
   if (message === undefined) return undefined;
 
   return message.replace(searchValue, (original, key) => {
-    // TODO: remove this piece of code in the next major version.
-    if (multipleValues.test(key)) {
-      const matches = multipleValues.exec(key);
-      if (matches === null) return original;
+    const matches = multipleValues.exec(key);
 
-      const replacer = replacements?.[matches[1]];
-      if (typeof replacer === "number")
-        return replacer === 1 ? matches[2] : matches[3];
-
-      return original;
-    }
-
-    // TODO: replace this piece of code by:
-    // const matches = multipleValues.exec(key);
-    // if (matches !== null) {
-    if (multipleValuesV3.test(key)) {
-      const matches = multipleValues.exec(key);
-      if (matches === null) return original;
-
+    if (matches !== null) {
       const replacer = replacements?.[matches[1]];
       if (typeof replacer !== "boolean") return original;
-
       return replacer ? matches[2] : matches[3];
     }
 
