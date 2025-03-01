9a12987b546d73f8d1b5aa6a2913ce7a5752fd1f
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-01 13:16:58 -0300

    fix(test): remove .swc folder on error too

--- src/_bin/test.ts
+++ src/_bin/test.ts
@@ -2,10 +2,10 @@ import run from "./_run";
 import { execute } from "./utils";
 
 export default async function test(): Promise<void> {
-  await run(
-    false,
-    () => execute("jest --passWithNoTests", true),
-    () => execute("del .swc", true),
+  await run(false, () =>
+    execute("jest --passWithNoTests", true).finally(() =>
+      execute("del .swc", true),
+    ),
   );
 }
 
