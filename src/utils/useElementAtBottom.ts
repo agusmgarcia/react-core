078447fadf0d640230f54ee33ede053b2329f39a
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-14 11:09:29 -0300

    fix(useElementAtTop): handle scenarios where element is initially null

--- src/utils/useElementAtBottom.ts
+++ src/utils/useElementAtBottom.ts
@@ -1,48 +1,38 @@
 import { useEffect, useState } from "react";
 
+import useDevicePixelRatio from "./useDevicePixelRatio";
+import useDimensions from "./useDimensions";
+
 export default function useElementAtBottom<TElement extends HTMLElement>(
   elementRef: React.RefObject<TElement>,
   initialValue = false,
 ): boolean {
   const [atBottom, setAtBottom] = useState(initialValue);
-  const [resize, setResize] = useState(false);
+
+  const devicePixelRatio = useDevicePixelRatio();
+  const dimensions = useDimensions(elementRef);
 
   useEffect(() => {
     const element = elementRef.current;
-    if (!element) return;
-
-    const set = (e: TElement): void =>
-      setAtBottom(
-        Math.abs(e.scrollTop - (e.scrollHeight - e.offsetHeight)) <
-          window.devicePixelRatio,
-      );
-
-    function handle(event: Event) {
-      const target = event.target;
-      if (!target) return;
-      if (!("scrollTop" in target)) return;
-      if (typeof target.scrollTop !== "number") return;
-      if (!("scrollHeight" in target)) return;
-      if (typeof target.scrollHeight !== "number") return;
-      if (!("offsetHeight" in target)) return;
-      if (typeof target.offsetHeight !== "number") return;
-      set(target as TElement);
+    if (!element) {
+      setAtBottom(initialValue);
+      return;
     }
 
-    set(element);
+    function handle(event: Event | { target: TElement }) {
+      const target = event.target as TElement;
+      setAtBottom(
+        Math.abs(
+          target.scrollTop - (target.scrollHeight - target.offsetHeight),
+        ) < devicePixelRatio,
+      );
+    }
+
+    handle({ target: element });
 
     element.addEventListener("scroll", handle);
     return () => element.removeEventListener("scroll", handle);
-  }, [elementRef, resize]);
-
-  useEffect(() => {
-    const element = elementRef.current;
-    if (!element) return;
-
-    const observer = new ResizeObserver(() => setResize((prev) => !prev));
-    observer.observe(element);
-    return () => observer.unobserve(element);
-  }, [elementRef]);
+  }, [devicePixelRatio, dimensions, elementRef, initialValue]);
 
   return atBottom;
 }
