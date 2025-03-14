eb1355436a3b07ab1779d5ff8269aaf695830055
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-14 11:08:00 -0300

    feat(useDimensions): add hook

---
+++ src/utils/useDimensions.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+type Dimensions = { height: number; width: number };
+
+const initialDimensions: Dimensions = { height: 0, width: 0 };
+
+export default function useDimensions<TElement extends Element>(
+  elementRef: React.RefObject<TElement>,
+  initialValue = initialDimensions,
+  options?: ResizeObserverOptions["box"],
+): Dimensions {
+  const [visible, setVisible] = useState(false);
+
+  const [dimensions, setDimensions] = useState(initialValue);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    setVisible(!!elementRef.current);
+  });
+
+  useEffect(() => {
+    if (!visible) {
+      setDimensions(initialValue);
+      return;
+    }
+
+    const element = elementRef.current;
+    if (!element) {
+      setDimensions(initialValue);
+      return;
+    }
+
+    const observer = new ResizeObserver((entries) =>
+      setDimensions({
+        height: entries[0].contentRect.height,
+        width: entries[0].contentRect.width,
+      }),
+    );
+
+    observer.observe(element, { box: options });
+    return () => observer.unobserve(element);
+  }, [options, elementRef, visible, initialValue]);
+
+  return dimensions;
+}
