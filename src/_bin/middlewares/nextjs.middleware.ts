4b8dd5c388a442c50c7b9716591feeed51fb8066
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-21 09:17:53 -0300

    feat(nextjs.middleware): omit devIndicators

--- src/_bin/middlewares/nextjs.middleware.ts
+++ src/_bin/middlewares/nextjs.middleware.ts
@@ -48,6 +48,7 @@ const nextConfig = `const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 /** @type import('next').NextConfig */
 module.exports = (phase) => ({
   basePath: process.env.BASE_PATH,
+  devIndicators: false,
   output: phase === PHASE_PRODUCTION_BUILD ? "export" : undefined,
   reactStrictMode: true,
 });
