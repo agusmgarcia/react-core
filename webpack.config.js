643db9499f4b44c30edbcea721987c28790b5f84
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-29 00:42:32 -0300

    fix(webpack.middleware): add getCustomTransformers

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,7 @@ module.exports = [
                   declaration: false,
                   jsx: "react-jsx",
                 },
+                getCustomTransformers,
               },
             },
           ],
