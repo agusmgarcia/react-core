abd882d3958478386daa9e9f3080bef9f65b454f
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-29 14:23:04 -0300

    chore(createObjectWithPropertiesSorted): move it into a separated file

--- src/_bin/middlewares/packageJSON.middleware.ts
+++ src/_bin/middlewares/packageJSON.middleware.ts
@@ -1,6 +1,12 @@
 import { type AsyncFunc } from "#src/utils";
 
-import { execute, files, getPackageJSON, git } from "../utils";
+import {
+  createObjectWithPropertiesSorted,
+  execute,
+  files,
+  getPackageJSON,
+  git,
+} from "../utils";
 
 export default async function packageJSONMiddleware(
   next: AsyncFunc,
@@ -110,33 +116,4 @@ async function createPackageFile(): Promise<string> {
   ]);
 
   return JSON.stringify(newPackageJSON, undefined, 2);
-}
-
-function createObjectWithPropertiesSorted<TElement extends object>(
-  input: TElement,
-  preferred: (keyof TElement)[] = [],
-): TElement {
-  const sortKeys = (key1: string, key2: string): number => {
-    const indexOfKey1 = preferred.indexOf(key1 as keyof TElement);
-    const indexOfKey2 = preferred.indexOf(key2 as keyof TElement);
-
-    if (indexOfKey1 === -1 && indexOfKey2 === -1)
-      return +(key1 > key2) || -(key2 > key1);
-
-    if (indexOfKey1 === -1) return 1;
-    if (indexOfKey2 === -1) return -1;
-
-    return indexOfKey1 - indexOfKey2;
-  };
-
-  return Object.keys(input)
-    .sort(sortKeys)
-    .reduce((result, key) => {
-      const property = input[key as keyof TElement];
-      result[key as keyof TElement] =
-        typeof property !== "object" || !property
-          ? property
-          : createObjectWithPropertiesSorted(property);
-      return result;
-    }, {} as TElement);
 }
--- src/_bin/utils/index.ts
+++ src/_bin/utils/index.ts
@@ -1,4 +1,5 @@
 export * as args from "./args";
+export { default as createObjectWithPropertiesSorted } from "./createObjectWithPropertiesSorted";
 export { default as execute } from "./execute";
 export * as files from "./files";
 export * as folders from "./folders";
