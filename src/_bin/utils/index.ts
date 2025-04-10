630ae481e8abbb54efc8d859cc71a53a6e38ce9f
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-10 12:17:33 -0300

    chore(question): add utility

--- src/_bin/utils/index.ts
+++ src/_bin/utils/index.ts
@@ -6,3 +6,4 @@ export * as folders from "./folders";
 export { default as getCore } from "./getCore";
 export { default as getPackageJSON } from "./getPackageJSON";
 export * as git from "./git";
+export { default as question } from "./question";
