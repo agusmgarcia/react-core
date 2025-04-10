7320ee5ec967926df8d7294d652a5fed974261c2
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-10 16:52:36 -0300

    chore(filters): add ability to set custom comparator

--- src/utils/filters.test.ts
+++ src/utils/filters.test.ts
@@ -6,6 +6,14 @@ describe("filters", () => {
     expect(array.filter(filters.distinct)).toStrictEqual([17, 6, 95]);
   });
 
+  it("remos duplicated elements from array using a custom comparator", () => {
+    const array = [{ name: "John" }, { name: "Doe" }, { name: "John" }];
+    expect(array.filter(filters.distinct("deep"))).toStrictEqual([
+      { name: "John" },
+      { name: "Doe" },
+    ]);
+  });
+
   it("paginates the elements from array", () => {
     const array = [17, 6, 95, 6];
     expect(array.filter(filters.paginate(1, 2))).toStrictEqual([17, 6]);
