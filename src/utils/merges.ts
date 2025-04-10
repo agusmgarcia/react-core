7a873df229422e329cd630833319bfcf40f8af28
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-10 12:51:26 -0300

    chore(merges): add ability to concat, filter and sort arrays

--- src/utils/merges.ts
+++ src/utils/merges.ts
@@ -1,21 +1,61 @@
-import { distinct } from "./filters";
+import * as filters from "./filters";
 
 export function strict(target: unknown, source: unknown): any {
-  return merge(target, source, 0);
+  return merge(target, source, 0, false, false, false);
 }
 
-export function shallow(target: unknown, source: unknown, level = 1): any {
-  return merge(target, source, level);
+export function shallow(
+  target: unknown,
+  source: unknown,
+  options:
+    | number
+    | {
+        arrayConcat?: boolean;
+        arrayRemoveDuplicated?: boolean;
+        level?: number;
+        sort?: boolean;
+      } = 1,
+): any {
+  return merge(
+    target,
+    source,
+    typeof options === "number"
+      ? options
+      : typeof options.level === "number"
+        ? options.level
+        : 1,
+    typeof options === "number" ? false : !!options.sort,
+    typeof options === "number" ? false : !!options.arrayConcat,
+    typeof options === "number" ? false : !!options.arrayRemoveDuplicated,
+  );
 }
 
-export function deep(target: unknown, source: unknown): any {
-  return merge(target, source, undefined);
+export function deep(
+  target: unknown,
+  source: unknown,
+  options?: {
+    arrayConcat?: boolean;
+    arrayRemoveDuplicated?: boolean;
+    sort?: boolean;
+  },
+): any {
+  return merge(
+    target,
+    source,
+    undefined,
+    !!options?.sort,
+    !!options?.arrayConcat,
+    !!options?.arrayRemoveDuplicated,
+  );
 }
 
 function merge(
   target: unknown,
   source: unknown,
   level: number | undefined,
+  sort: boolean,
+  arrayConcat: boolean,
+  arrayRemoveDuplicated: boolean,
 ): any {
   if (!!level && level < 0) return source;
   if (target === source) return source;
@@ -24,27 +64,41 @@ function merge(
   if (Array.isArray(target)) {
     if (!Array.isArray(source)) return source;
 
-    const length = Math.max(target.length, source.length);
-    const result = new Array(length);
+    let result: any[];
 
-    for (let i = 0; i < length; i++)
-      result[i] =
-        i < source.length && i < target.length
-          ? merge(target[i], source[i], !!level ? level - 1 : undefined)
-          : i < source.length
-            ? source[i]
-            : target[i];
+    if (arrayConcat) result = [...target, ...source];
+    else {
+      const length = Math.max(target.length, source.length);
+      result = new Array(length);
 
-    return result;
+      for (let i = 0; i < length; i++)
+        result[i] =
+          i < source.length && i < target.length
+            ? merge(
+                target[i],
+                source[i],
+                !!level ? level - 1 : undefined,
+                sort,
+                arrayConcat,
+                arrayRemoveDuplicated,
+              )
+            : i < source.length
+              ? source[i]
+              : target[i];
+    }
+
+    return result
+      .sort(!sort ? () => 0 : undefined)
+      .filter(!arrayRemoveDuplicated ? () => true : filters.distinct("deep"));
   }
 
   if (typeof target === "object" && !!target) {
     if (Array.isArray(source)) return source;
     if (typeof source !== "object" || !source) return source;
 
-    const keys = [...Object.keys(target), ...Object.keys(source)].filter(
-      distinct,
-    );
+    const keys = [...Object.keys(target), ...Object.keys(source)]
+      .sort(!sort ? () => 0 : undefined)
+      .filter(filters.distinct);
 
     const result: Record<string, any> = {};
 
@@ -55,6 +109,9 @@ function merge(
               target[key as keyof typeof target],
               source[key as keyof typeof source],
               !!level ? level - 1 : undefined,
+              sort,
+              arrayConcat,
+              arrayRemoveDuplicated,
             )
           : key in source
             ? source[key as keyof typeof source]
