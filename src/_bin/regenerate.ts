a17a2a076f0b00d0c24c5b6b60c0ba034a08826f
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-10 12:14:27 -0300

    chore(regenerate): stop using echo in favor of console.log

--- src/_bin/regenerate.ts
+++ src/_bin/regenerate.ts
@@ -2,10 +2,10 @@ import run from "./_run";
 import { execute } from "./utils";
 
 export default async function regenerate(): Promise<void> {
-  await execute("echo Regenerating files...", true);
+  console.log("Regenerating files...");
   await run(true, () => Promise.resolve());
   await execute("npm run format", true);
-  await execute("echo Files regenerated!", true);
+  console.log("Files regenerated!");
 }
 
 regenerate();
