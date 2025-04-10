7dd71920167b59597b43a59fde2b2da27e4b1834
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-10 16:52:36 -0300

    chore(filters): add ability to set custom comparator

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,11 +1,47 @@
+import * as equals from "./equals";
 import type Func from "./Func.types";
 
 export function distinct<TElement>(
   element: TElement,
   index: number,
   array: TElement[],
-): boolean {
-  return array.indexOf(element) === index;
+): boolean;
+
+export function distinct<TElement>(
+  compare:
+    | "deep"
+    | "shallow"
+    | "strict"
+    | Func<boolean, [element1: TElement, element2: TElement]>,
+): Func<boolean, [element: TElement, index: number, array: TElement[]]>;
+
+export function distinct<TElement>(
+  elementOrCompare:
+    | TElement
+    | "deep"
+    | "shallow"
+    | "strict"
+    | Func<boolean, [element1: TElement, element2: TElement]>,
+  index?: number,
+  array?: TElement[],
+):
+  | boolean
+  | Func<boolean, [element: TElement, index: number, array: TElement[]]> {
+  if (typeof index === "number" && !!array)
+    return array.indexOf(elementOrCompare as TElement) === index;
+
+  return (element1, index, array) =>
+    array.findIndex((element2) =>
+      elementOrCompare === "deep"
+        ? equals.deep(element1, element2)
+        : elementOrCompare === "shallow"
+          ? equals.shallow(element1, element2)
+          : elementOrCompare === "strict"
+            ? equals.strict(element1, element2)
+            : elementOrCompare instanceof Function
+              ? elementOrCompare(element1, element2)
+              : false,
+    ) === index;
 }
 
 export function paginate<TElement>(
