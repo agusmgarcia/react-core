318eeded5a3739e05adc19dbe9de61703873d36d
Agus Garcia <agusss.garcia19@gmail.com>
2025-04-28 14:18:26 -0300

    fix(_app.tsx): consider base path when locating favicon

--- src/_bin/middlewares/nextjs.middleware.ts
+++ src/_bin/middlewares/nextjs.middleware.ts
@@ -70,6 +70,11 @@ export default function App({ Component }: AppProps<any>) {
     <>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          href={\`\${process.env.NEXT_PUBLIC_BASE_PATH || ""}/favicon.ico\`}
+          rel="icon"
+          type="image/x-icon"
+        />
       </Head>
 
       <Component />
